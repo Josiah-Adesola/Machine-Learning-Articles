f7b0911478f15e59da685a76d306b0740c353ff1
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-04-14 03:21:47 +0100

    Added pdf and python script

--- Python-Automations/pdfmerge.py
+++ Python-Automations/pdfmerge.py
@@ -1,1 +1,9 @@
+from PyPDF2 import PdfWriter
 
+merger = PdfWriter()
+
+for pdf in ["Chap8_Laplace.pdf", "Chap8_Laplace_sol.pdf"]:
+    merger.append(pdf)
+
+merger.write("merged-pdf.pdf")
+merger.close()
